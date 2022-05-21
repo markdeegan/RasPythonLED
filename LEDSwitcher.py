370051f1de4d38d357c8b86492422e7dc9bc6ef3
markdeegan <mark@deegan.net>
2022-05-21 13:18:13 +0100

    Adding existing LED Switcher and MD constants file

---
+++ LEDSwitcher.py
@@ -0,0 +1,59 @@
+from tkinter import *
+import tkinter.font
+import RPi.GPIO as GPIO
+# this is a module I defined to alias GPIO numbers to Raspberry Pi pins
+import MDRPiGPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+# set pin 40 as an output pin, likely to draw some current
+GPIO.setup(40, GPIO.OUT)
+# set the initial status of pin 40 to LOW 
+GPIO.output(40, GPIO.LOW)
+
+# create a new window called win
+win = Tk()
+
+# set the font for the GUI elements
+myFont = tkinter.font.Font(family = 'Helvetica', size = 36, weight = 'bold')
+
+# define the ledON function
+def ledON():
+	# print that the LED button has been pressed
+	print("LED button pressed")
+	# check the status of pin 40, and respond accordingly
+	# if it is HIGH, then 
+	if GPIO.input(40):
+		# set status of pin 40 to LOW and 
+		GPIO.output(40,GPIO.LOW)
+		# change the text displayed by the button to LED ON
+		ledButton["text"] = "LED ON"
+	# if it is LOW (else) then
+	else:
+		# set status of pin 40 to LOW and 
+		GPIO.output(40,GPIO.HIGH)
+		# change the text displayed by the button to LED OFF
+		ledButton["text"] = "LED OFF"
+
+# define the exitProgram function
+def exitProgram():
+	print("Exit Button pressed")
+	GPIO.cleanup()
+	win.quit()
+
+
+# set the title of the application window
+win.title("LED Switcher")
+# set the size of the app (to match the scree size, make this dynamic later)
+win.geometry('800x480')
+
+# define a button, called eitButton, and set various parameters
+exitButton  = Button(win, text = "Exit", font = myFont, command = exitProgram, height =2 , width = 6) 
+exitButton.pack(side = BOTTOM)
+
+# define a button called ledButton and set various parameters
+ledButton = Button(win, text = "LED ON", font = myFont, command = ledON, height = 2, width =8 )
+ledButton.pack()
+
+mainloop()
---
+++ MDRPiGPIO.py
@@ -0,0 +1,36 @@
+from typing import Final
+
+# this is a module I defined to alias GPIO numbers to Raspberry Pi pins
+# for example, to define a constant for GPIO 21 = Pin 40
+
+GPIO0: Final[int] = 27
+GPIO1: Final[int] = 28
+GPIO2: Final[int] = 3
+GPIO3: Final[int] = 5
+GPIO4: Final[int] = 7
+GPIO5: Final[int] = 29
+GPIO6: Final[int] = 31
+GPIO7: Final[int] = 26
+GPIO8: Final[int] = 24	
+GPIO9: Final[int] = 21
+GPIO10: Final[int] = 19
+GPIO11: Final[int] = 23
+GPIO12: Final[int] = 32
+GPIO13: Final[int] = 33
+GPIO14: Final[int] = 8
+GPIO15: Final[int] = 10
+GPIO16: Final[int] = 36
+GPIO17: Final[int] = 11
+GPIO18: Final[int] = 12
+GPIO19: Final[int] = 35
+GPIO20: Final[int] = 38
+GPIO21: Final[int] = 40
+GPIO22: Final[int] = 15
+GPIO23: Final[int] = 16
+GPIO24: Final[int] = 18
+GPIO25: Final[int] = 22
+GPIO26: Final[int] = 37
+GPIO27: Final[int] = 13
+
+# define a constant for GPIO 27 = Pin 40
+GPIO27: Final[int] = 40
