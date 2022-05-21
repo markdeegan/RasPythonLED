d327b4cdeaa3fa5005dc5da409e8f281a71818c6
markdeegan <mark@deegan.net>
2022-05-21 13:52:51 +0100

    Adding some comments to see push operation

--- LEDSwitcher.py
+++ LEDSwitcher.py
@@ -5,6 +5,8 @@ MD20220521-01 LEDSwitcher.py
 This code is a sample to test the development of a touch-screen GUI
 on a 3.5" LED display on a Raspbeery Pi.
 
+Pyton development of a GUI that works on the 3.5" LED screen on a Raspberry Pi 3B
+
 """
 from tkinter import *
 import tkinter.font
